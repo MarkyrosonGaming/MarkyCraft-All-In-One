c6e6a8964af806620fb0ff34e8f13e768e118f11
Markyroson <Markyroson@users.noreply.github.com>
2016-12-10 21:04:59 -0800

    Cleaned code (#2) (#3)
    * Updated comments and documentation (#2) (#1)
    
    * Removed irrelevant content
    
    * Improved comments
    
    * Improved comments
    
    * Improved comment style
    
    * Updated comments/documentation & more
    
    Updated comments/documentation and added TODO's
    
    * Added TODO
    
    * Major cleaning up of code
    
    Begun transition of code from class spaghetti to cleaned up with methods
    in place.
    
    * Completed conversion
    
    Completed conversion from spaghetti classes.
    
    * Added documentation
    
    Added documentation and removed unused code.

--- src/me/Markyroson/MarkyCraft/Stores.java
+++ src/me/Markyroson/MarkyCraft/Stores.java
@@ -1,6 +1,11 @@
 package me.Markyroson.MarkyCraft;
 
 import me.Markyroson.MarkyCraft.lib.Names;
+import me.Markyroson.MarkyCraft.lib.Names.Info;
+import me.Markyroson.MarkyCraft.lib.Names.Inventories;
+import me.Markyroson.MarkyCraft.lib.Names.ShopItemsWithTwoLore;
+import me.Markyroson.MarkyCraft.lib.Names.Shop;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
@@ -9,58 +14,46 @@ import org.bukkit.inventory.Inventory;
 * @author Markyroson
 */
 public class Stores {
-	public static Inventory shop;	//Creates shop inventory
-	public static Inventory navigator; //Creates server navigator inventory
-	public static Inventory books;	//Creates books inventory
-	
-	private static Names.Items.Shop.Bedrock bedrock;
-	private static Names.Items.Shop.EndStone endstone;
-	private static Names.Items.Shop.Obsidian obsidian;
-	private static Names.Items.Shop.WaterBucket waterbucket;
-	private static Names.Items.Shop.Netherrack netherrack;
-	private static Names.Items.Shop.Sand sand;
-	private static Names.Items.Shop.Grass grass;
-	private static Names.Items.Shop.Ice ice;
-	private static Names.Items.Shop.PackedIce packedice;
-	private static Names.Items.Shop.CobbleStone cobblestone;
-	private static Names.Items.Shop.GlowStone glowstone;
-	private static Names.Items.Shop.Dirt dirt;
-	private static Names.Items.Shop.Stone stone;
-	private static Names.Items.HubPotato hubpotato;
-	private static Names.Items.Books.WebBook webBook;
-	
+	public static Inventory shop;	// Creates shop inventory
+	public static Inventory navigator; // Creates server navigator inventory
+	public static Inventory books;	// Creates books inventory
+		
 	public final static class Setups
 	{
-		@SuppressWarnings("static-access")
-		public static void Shop()	//Shop/store setup
+		public static void Shop()	// Shop/store setup
 		{
-			shop = Bukkit.createInventory(null, Names.Info.Shop.size, Names.Info.Shop.name);
-			shop.setItem(0, Api.createItem(Material.APPLE, 1, 0, Names.Items.Shop.Apple.name, Names.Items.Shop.Apple.lore, Names.Items.Shop.Apple.lore2));
-			shop.setItem(1, Api.createItem(Material.WATER_BUCKET, 1, 0, waterbucket.name, waterbucket.lore, waterbucket.lore2));
-			shop.setItem(2, Api.createItem(Material.OBSIDIAN, 64, 0, obsidian.name, obsidian.lore1, obsidian.lore2));
-			shop.setItem(3, Api.createItem(Material.ENDER_STONE, 32, 0, endstone.name, endstone.lore1, endstone.lore2));
-			shop.setItem(4, Api.createItem(Material.NETHERRACK, 64, 0, netherrack.name, netherrack.lore1, netherrack.lore2));
-			shop.setItem(5, Api.createItem(Material.GLOWSTONE, 16, 0, glowstone.name, glowstone.lore1, glowstone.lore2));
-			shop.setItem(6, Api.createItem(Material.SAND, 64, 0, sand.name, sand.lore, sand.lore2));
-			shop.setItem(7, Api.createItem(Material.COBBLESTONE, 64, 0, cobblestone.name, cobblestone.lore, cobblestone.lore2));
-			shop.setItem(8, Api.createItem(Material.STONE, 64, 0, stone.name, stone.lore, stone.lore2));
-			shop.setItem(9, Api.createItem(Material.DIRT, 64, 0, dirt.name, dirt.lore, dirt.lore2));
-			shop.setItem(10, Api.createItem(Material.GRASS, 64, 0, grass.name, grass.lore, grass.lore2));
-			shop.setItem(11, Api.createItem(Material.ICE, 16, 0, ice.name, ice.lore, ice.lore2));
-			shop.setItem(12, Api.createItem(Material.PACKED_ICE, 16, 0, packedice.name, packedice.lore, packedice.lore2));
-			shop.setItem(13, Api.createItem(Material.BEDROCK, 1, 0, bedrock.name, bedrock.lore, null));
+			shop = Bukkit.createInventory(null, Info.getInvSize(Inventories.SHOP), Info.getName(Inventories.SHOP));
+			shop.setItem(0, Api.createItem(Material.APPLE, 1, 0, Names.getItemName(ShopItemsWithTwoLore.APPLE), Names.getLore(ShopItemsWithTwoLore.APPLE), Names.getLore2(ShopItemsWithTwoLore.APPLE)));
+			shop.setItem(1, Api.createItem(Material.WATER_BUCKET, 1, 0, Names.getItemName(ShopItemsWithTwoLore.WATER_BUCKET), Names.getLore(ShopItemsWithTwoLore.WATER_BUCKET), Names.getLore2(ShopItemsWithTwoLore.WATER_BUCKET)));
+			shop.setItem(2, Api.createItem(Material.OBSIDIAN, 64, 0, Names.getItemName(ShopItemsWithTwoLore.OBSIDIAN), Names.getLore(ShopItemsWithTwoLore.OBSIDIAN), Names.getLore2(ShopItemsWithTwoLore.OBSIDIAN)));
+			shop.setItem(3, Api.createItem(Material.ENDER_STONE, 32, 0, Names.getItemName(ShopItemsWithTwoLore.END_STONE), Names.getLore(ShopItemsWithTwoLore.END_STONE), Names.getLore2(ShopItemsWithTwoLore.END_STONE)));
+			shop.setItem(4, Api.createItem(Material.NETHERRACK, 64, 0, Names.getItemName(ShopItemsWithTwoLore.NETHERRACK), Names.getLore(ShopItemsWithTwoLore.NETHERRACK), Names.getLore2(ShopItemsWithTwoLore.NETHERRACK)));
+			shop.setItem(5, Api.createItem(Material.GLOWSTONE, 16, 0, Names.getItemName(ShopItemsWithTwoLore.GLOW_STONE), Names.getLore(ShopItemsWithTwoLore.GLOW_STONE), Names.getLore2(ShopItemsWithTwoLore.GLOW_STONE)));
+			shop.setItem(6, Api.createItem(Material.SAND, 64, 0, Names.getItemName(ShopItemsWithTwoLore.SAND), Names.getLore(ShopItemsWithTwoLore.SAND), Names.getLore2(ShopItemsWithTwoLore.SAND)));
+			shop.setItem(7, Api.createItem(Material.COBBLESTONE, 64, 0, Names.getItemName(ShopItemsWithTwoLore.COBBLE_STONE), Names.getLore(ShopItemsWithTwoLore.COBBLE_STONE), Names.getLore2(ShopItemsWithTwoLore.COBBLE_STONE)));
+			shop.setItem(8, Api.createItem(Material.STONE, 64, 0, Names.getItemName(ShopItemsWithTwoLore.STONE), Names.getLore(ShopItemsWithTwoLore.STONE), Names.getLore2(ShopItemsWithTwoLore.STONE)));
+			shop.setItem(9, Api.createItem(Material.DIRT, 64, 0, Names.getItemName(ShopItemsWithTwoLore.DIRT), Names.getLore(ShopItemsWithTwoLore.DIRT), Names.getLore2(ShopItemsWithTwoLore.DIRT)));
+			shop.setItem(10, Api.createItem(Material.GRASS, 64, 0, Names.getItemName(ShopItemsWithTwoLore.GRASS), Names.getLore(ShopItemsWithTwoLore.GRASS), Names.getLore2(ShopItemsWithTwoLore.GRASS)));
+			shop.setItem(11, Api.createItem(Material.ICE, 16, 0, Names.getItemName(ShopItemsWithTwoLore.ICE), Names.getLore(ShopItemsWithTwoLore.ICE), Names.getLore2(ShopItemsWithTwoLore.ICE)));
+			shop.setItem(12, Api.createItem(Material.PACKED_ICE, 16, 0, Names.getItemName(ShopItemsWithTwoLore.PACKED_ICE), Names.getLore(ShopItemsWithTwoLore.PACKED_ICE), Names.getLore2(ShopItemsWithTwoLore.PACKED_ICE)));
+			shop.setItem(13, Api.createItem(Material.BEDROCK, 1, 0, Names.getItemName(ShopItemsWithTwoLore.BEDROCK), Names.getLore(ShopItemsWithTwoLore.BEDROCK)));
 		}
-		@SuppressWarnings("static-access")
-		public static void ServerNavigator()	//ServerNavigator setup
+		/**
+		* This method is in charge of setting up Server Navigator
+		*/
+		public static void ServerNavigator()
 		{
-			navigator = Bukkit.createInventory(null, Names.Info.ServerNavigator.size, Names.Info.ServerNavigator.name);
-			navigator.setItem(0, Api.createItem(Material.BAKED_POTATO, 1, 0, hubpotato.name, hubpotato.lore, null));
+			navigator = Bukkit.createInventory(null, Info.getInvSize(Inventories.SERVER_NAVIGATOR), Info.getName(Inventories.SERVER_NAVIGATOR));
+			navigator.setItem(0, Api.createItem(Material.BAKED_POTATO, 1, 0, Names.getItemName(Shop.HUB_POTATO), Names.getLore(Shop.HUB_POTATO)));
 		}
-		@SuppressWarnings("static-access")
-		public static void Books()	//Books setup
+		
+		/**
+		* This method is in charge of setting up books
+		*/
+		public static void Books()
 		{
-			books = Bukkit.createInventory(null, Names.Info.Books.size, Names.Info.Books.name);
-			books.setItem(0, Api.createItem(Material.BOOK, 1, 0, webBook.name, webBook.lore, null));
+			books = Bukkit.createInventory(null, Info.getInvSize(Inventories.BOOKS), Info.getName(Inventories.BOOKS));
+			books.setItem(0, Api.createItem(Material.BOOK, 1, 0, Names.getItemName(Shop.BOOK_WEB_BOOK), Names.getLore(Shop.BOOK_WEB_BOOK)));
 		}
 	}
 	/**
--- src/me/Markyroson/MarkyCraft/lib/Books.java
+++ src/me/Markyroson/MarkyCraft/lib/Books.java
@@ -1,25 +1,81 @@
 package me.Markyroson.MarkyCraft.lib;
 
 public class Books {
-	public static final class Setup
+	/**
+	 * Books
+	 * @author Markyroson
+	 *
+	 */
+	public static enum BookType
 	{
-		public static final class WebBook
+		WEB_BOOK
+	}
+	/**
+	 * Pages of Web Book
+	 * @author Markyroson
+	 *
+	 */
+	public static enum Web_BookPages
+	{
+		PAGE1,
+		PAGE2,
+		PAGE3,
+		PAGE4
+	}
+	
+	/**
+	 * Get page from book
+	 * @author Markyroson
+	 * @param book Book to get page from
+	 * @param page Page to get
+	 * @return Page contents
+	 */
+	public static String getBookPage(BookType book, Web_BookPages page)
+	{
+		if(book == BookType.WEB_BOOK)
 		{
-			public static final class Pages
+			switch(page)
 			{
-				public static final String page1 = "Hello! This is the book of web addresses!";
-				public static final String page2 = "http://www.youtube.com/Markyroson";
-				public static final String page3 = "http://www.twitter.com/Markyroson";
-				public static final String page4 = "http://www.facebook.com/Markyroson";
+				case PAGE1:
+					return "Hello! This is the book of web addresses!";
+				case PAGE2:
+					return "http://www.youtube.com/Markyroson";
+				case PAGE3:
+					return "http://www.twitter.com/Markyroson";
+				case PAGE4:
+					return "http://www.facebook.com/Markyroson";
+				default:
+					return "Page not found";
 			}
-			public static final class Author
-			{
-				public static final String name = "The Server";	//book author
-			}
-			public static final class Title
-			{
-				public static final String name = "Websites";	//book title
-			}
-		}	//WebBook class end bracket
-	}	//Setup class end bracket
+		}
+		return null;
+	}
+	/**
+	 * Get book author(s)
+	 * @author Markyroson
+	 * @param book Book to get author(s) of
+	 * @return Author(s)
+	 */
+	public static String getAuthor(BookType book)
+	{
+		if(book == BookType.WEB_BOOK)
+		{
+			return "The Server";
+		}
+		return null;
+	}
+	/**
+	 * Get book title
+	 * @author Markyroson
+	 * @param book Book to get title of
+	 * @return Book title
+	 */
+	public static String getTitle(BookType book)
+	{
+		if(book == BookType.WEB_BOOK)
+		{
+			return "Websites";
+		}
+		return null;
+	}
 }	//End of file
