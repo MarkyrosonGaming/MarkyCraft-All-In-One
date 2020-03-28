b530cc24f6c00f55fd47683532c21a60c2921ce7
Markyroson <markyroson@gmail.com>
2020-03-28 00:04:56 -0700

    Format

--- src/main/java/me/markyroson/markycraftallinone/Listeners.java
+++ src/main/java/me/markyroson/markycraftallinone/Listeners.java
@@ -28,11 +28,10 @@ public class Listeners implements Listener {
         UUID uuid = e.getPlayer().getUniqueId();
         Player p = e.getPlayer();
 
-        if (!plugin.getConfig().contains(uuid.toString())) {
+        if (!plugin.getConfig().contains(uuid.toString()))
             plugin.getMoney().put(uuid, 0);
-        } else {
+        else
             plugin.getMoney().put(uuid, plugin.getConfig().getInt(p.getUniqueId() + ".Silver"));
-        }
     }
 
     @EventHandler
@@ -42,22 +41,20 @@ public class Listeners implements Listener {
                 Player p = e.getEntity().getKiller();
                 //plugin.getApi().giveSilver(p, 200);
                 EconomyResponse r = Main.econ.depositPlayer(p, 200);
-                if(r.transactionSuccess()) {
+                if(r.transactionSuccess())
                     p.sendMessage(String.format("You killed a monster! Good job! :-) %s and now have %s", Main.econ.format(r.amount), Main.econ.format(r.balance)));	//send success message &^ tell player what they spent/remaining balance
-                } else {
+                else
                     p.sendMessage(String.format("An error occured: %s", r.errorMessage));	//send error message
-                }
             }
         } else if (e.getEntity() instanceof Villager) {
             if (e.getEntity().getKiller() != null) {
                 Player p = e.getEntity().getKiller();
                 //plugin.getApi().takeSilver(p, 200);
                 EconomyResponse r = Main.econ.withdrawPlayer(p, 200);
-                if(r.transactionSuccess()) {
+                if(r.transactionSuccess())
                     p.sendMessage(String.format("You killed a villager! :-( That will cost you %s and now have %s", Main.econ.format(r.amount), Main.econ.format(r.balance)));	//send success message &^ tell player what they spent/remaining balance
-                } else {
+                else
                     p.sendMessage(String.format("An error occured: %s", r.errorMessage));	//send error message
-                }
             }
         }
     }
@@ -82,20 +79,16 @@ public class Listeners implements Listener {
                     }
 
                     if (balance >= 5){
-                        ItemStack apple = new ItemStack(Material.APPLE, 1);	//create itemstack to give player
-                        if (event.getClick().isLeftClick()) {
-                            transact(p, apple, "apple", 5.00, true);
-                        }
+                        if (event.getClick().isLeftClick())
+                            transact(p, new ItemStack(Material.APPLE, 1), "apple", 5.00, true);
                     } else {
                         p.sendMessage("You don't have enough money!");
                     }
                     if (event.getClick().isRightClick()) {
-                        if (p.getInventory().contains(Material.APPLE)) {
+                        if (p.getInventory().contains(Material.APPLE))
                             transact(p, new ItemStack(Material.APPLE, 1), "apple", 5.00, false);
-                        }
-                        else {
+                        else
                             p.sendMessage("You don't have enough apples to sell!");
-                        }
                     }
                 }
             }
@@ -104,17 +97,14 @@ public class Listeners implements Listener {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
                 if (balance >= 50){
-                    ItemStack waterBucket = new ItemStack(Material.WATER_BUCKET, 1);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, waterBucket, "Water Bucket", 50.00, true);
-                    }
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.WATER_BUCKET, 1), "Water Bucket", 50.00, true);
                 } else {
                     p.sendMessage("You don't have enough money!");
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.WATER_BUCKET)) {
+                    if (p.getInventory().contains(Material.WATER_BUCKET))
                         p.sendMessage("You can't sell a Water Bucket!");
-                    }
                 }
             }
             //Lava Water
@@ -122,15 +112,12 @@ public class Listeners implements Listener {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
                 if (balance >= 75){
-                    ItemStack lavaBucket = new ItemStack(Material.LAVA_BUCKET, 1);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, lavaBucket, "Lava Bucket", 75.00, true);
-                    }
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.LAVA_BUCKET, 1), "Lava Bucket", 75.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.LAVA_BUCKET)) {
+                    if (p.getInventory().contains(Material.LAVA_BUCKET))
                         p.sendMessage("You can't sell a Lava Bucket!");
-                    }
                 }
             }
             //Obsidian
@@ -138,18 +125,14 @@ public class Listeners implements Listener {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
                 if (balance >= 175){
-                    ItemStack obsidian = new ItemStack(Material.OBSIDIAN, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, obsidian, "obsidian", 175.00, true);
-                    }
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.OBSIDIAN, 64), "obsidian", 175.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.OBSIDIAN)) {
+                    if (p.getInventory().contains(Material.OBSIDIAN))
                         transact(p, new ItemStack(Material.OBSIDIAN, 64), "Obsidian", 125.00, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Obsidian to sell!");
-                    }
                 }
             }
             //EndStone
@@ -157,37 +140,29 @@ public class Listeners implements Listener {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
                 if (balance >= 150){
-                    ItemStack endStone = new ItemStack(Material.END_STONE, 32);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, endStone, "End Stone", 150.00, true);
-                    }
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.END_STONE, 32), "End Stone", 150.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.END_STONE)) {
+                    if (p.getInventory().contains(Material.END_STONE))
                         transact(p, new ItemStack(Material.END_STONE, 32), "End Stone", 50.00, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough End Stone to sell!");
-                    }
                 }
             }
             //Netherrack
             if (item != null && item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Netherrack")) {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
-                if (balance >= 62.50){
-                    ItemStack netherRack = new ItemStack(Material.NETHERRACK, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Netherrack", 62.50, true);
-                    }
+                if (balance >= 62.50) {
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.NETHERRACK, 64), "Netherrack", 62.50, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.NETHERRACK)) {
+                    if (p.getInventory().contains(Material.NETHERRACK))
                         transact(p, new ItemStack(Material.NETHERRACK, 64), "Netherrack", 25.00, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Netherrack to sell!");
-                    }
                 }
             }
             //Glowstone
@@ -195,37 +170,29 @@ public class Listeners implements Listener {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
                 if (balance >= 100){
-                    ItemStack stack = new ItemStack(Material.GLOWSTONE, 16);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, stack, "Glowstone", 100.00, true);
-                    }
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.GLOWSTONE, 16), "Glowstone", 100.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.GLOWSTONE)) {
+                    if (p.getInventory().contains(Material.GLOWSTONE))
                         transact(p, new ItemStack(Material.GLOWSTONE, 16), "Glowstone", 10.00, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Glowstone to sell!");
-                    }
                 }
             }
             //Gravel
             if (item != null && item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Gravel")) {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
-                if (balance >= 75){
-                    ItemStack netherRack = new ItemStack(Material.GRAVEL, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Gravel", 75.00, true);
-                    }
+                if (balance >= 75) {
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.GRAVEL, 64), "Gravel", 75.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.GRAVEL)) {
+                    if (p.getInventory().contains(Material.GRAVEL))
                         transact(p, new ItemStack(Material.GRAVEL, 64), "Gravel", 12.50, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Gravel to sell!");
-                    }
                 }
             }
             //Sand
@@ -233,56 +200,44 @@ public class Listeners implements Listener {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
                 if (balance >= 75){
-                    ItemStack netherRack = new ItemStack(Material.SAND, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Sand", 75.00, true);
-                    }
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.SAND, 64), "Sand", 75.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.SAND)) {
+                    if (p.getInventory().contains(Material.SAND))
                         transact(p, new ItemStack(Material.SAND, 64), "Sand", 17.50, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Sand to sell!");
-                    }
                 }
             }
             //Cobblestone
             if (item != null && item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Cobblestone")) {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
-                if (balance >= 100){
-                    ItemStack netherRack = new ItemStack(Material.COBBLESTONE, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Cobblestone", 100.00, true);
-                    }
+                if (balance >= 100) {
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.COBBLESTONE, 64), "Cobblestone", 100.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.COBBLESTONE)) {
+                    if (p.getInventory().contains(Material.COBBLESTONE))
                         transact(p, new ItemStack(Material.COBBLESTONE, 64), "Gravel", 25.00, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Cobblestone to sell!");
-                    }
                 }
             }
             //Stone
             if (item != null && item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Stone")) {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
-                if (balance >= 137.50){
-                    ItemStack netherRack = new ItemStack(Material.STONE, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Stone", 137.50, true);
-                    }
+                if (balance >= 137.50) {
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.STONE, 64), "Stone", 137.50, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.STONE)) {
+                    if (p.getInventory().contains(Material.STONE))
                         transact(p, new ItemStack(Material.STONE, 64), "Stone", 37.50, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Stone to sell!");
-                    }
                 }
             }
             //Dirt
@@ -290,48 +245,38 @@ public class Listeners implements Listener {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
                 if (balance >= 75){
-                    ItemStack netherRack = new ItemStack(Material.DIRT, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Dirt", 75.00, true);
-                    }
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.DIRT, 64), "Dirt", 75.00, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.DIRT)) {
+                    if (p.getInventory().contains(Material.DIRT))
                         transact(p, new ItemStack(Material.DIRT, 64), "Dirt", 75.00, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Dirt to sell!");
-                    }
                 }
             }
             //Grass
             if (item != null && item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Grass")) {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
-                if (balance >= 75){
-                    ItemStack netherRack = new ItemStack(Material.GRASS, 64);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Grass", 92.50, true);
-                    }
+                if (balance >= 75) {
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.GRASS, 64), "Grass", 92.50, true);
                 }
                 if (event.getClick().isRightClick()) {
-                    if (p.getInventory().contains(Material.GRASS)) {
+                    if (p.getInventory().contains(Material.GRASS))
                         transact(p, new ItemStack(Material.GRASS, 64), "Grass", 92.50, false);
-                    }
-                    else {
+                    else
                         p.sendMessage("You don't have enough Grass to sell!");
-                    }
                 }
             }
             //Ice
             if (item != null && item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "Ice")) {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
-                if (balance >= 150){
-                    ItemStack netherRack = new ItemStack(Material.ICE, 16);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Ice", 150.00, true);
-                    }
+                if (balance >= 150) {
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.ICE, 16), "Ice", 150.00, true);
                 }
                 if (event.getClick().isRightClick()) {
                     if (p.getInventory().contains(Material.ICE)) {
@@ -343,11 +288,9 @@ public class Listeners implements Listener {
             if (item != null && item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "PackedIce")) {
                 Player p = (Player) event.getWhoClicked();
                 Double balance = Main.econ.getBalance(p);
-                if (balance >= 75){
-                    ItemStack netherRack = new ItemStack(Material.PACKED_ICE, 16);	//create itemstack to give player
-                    if (event.getClick().isLeftClick()) {
-                        transact(p, netherRack, "Packed Ice", 75.00, true);
-                    }
+                if (balance >= 75) {
+                    if (event.getClick().isLeftClick())
+                        transact(p, new ItemStack(Material.PACKED_ICE, 16), "Packed Ice", 75.00, true);
                 }
                 if (event.getClick().isRightClick()) {
                     if (p.getInventory().contains(Material.PACKED_ICE)) {
